a8a4db28800b787d78116d05f00ce10532382b1a
AnnaVeller <anna.ovsi@mail.ru>
2021-06-06 17:58:27 +0300

    homework-3

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
+const arena = document.getElementsByClassName('arenas arena1')[0];
+const randomBtn = document.querySelector('.button');
+randomBtn.addEventListener('click', event => {
+    changeHP(player1);
+    changeHP(player2);
+});
+
+
 const player1 = {
     name: 'Shrek',
-    hp: 99,
+    hp: 100,
+    player: 1,
     img: "http://reactmarathon-api.herokuapp.com/assets/scorpion.gif",
     weapon: ['Нож', 'Бомба', 'Пук'],
     attack() {
@@ -11,6 +20,7 @@ const player1 = {
 const player2 = {
     name: 'Fiona',
     hp: 100,
+    player: 2,
     img: "http://reactmarathon-api.herokuapp.com/assets/kitana.gif",
     weapon: ['Нож', 'Бомба', 'Пук'],
     attack() {
@@ -18,17 +28,15 @@ const player2 = {
     }
 };
 
-function createPlayer(className) {
-    let player;
-    if (className === 'player1') {
-        player = player1;
-    }
-    if (className === 'player2') {
-        player = player2;
-    }
 
-    const div = document.createElement('div');
-    div.className = className;
+const div1 = createPlayer(player1);
+const div2 = createPlayer(player2);
+
+arena.appendChild(div1);
+arena.appendChild(div2);
+
+function createPlayer(player) {
+    div = createElement('div', player.player === 1 ? 'player1' : 'player2')
     div.innerHTML =
         `<div class="progressbar">
                 <div class="life"></div>
@@ -38,11 +46,40 @@ function createPlayer(className) {
                 <img src="${player.img}">
          </div>`;
     return div;
-
 }
 
-const div1 = createPlayer('player1', player1);
-const div2 = createPlayer('player2', player2);
 
-document.getElementsByClassName('arenas arena1')[0].appendChild(div1);
-document.getElementsByClassName('arenas arena1')[0].appendChild(div2);
+function changeHP(player) {
+    const playerLife = document.querySelector('.player' + player.player + ' .life');
+    player.hp -= getRandom(1, 20);
+    playerLife.style.width = player.hp + '%';
+
+    if (player.hp < 0) {
+        player.hp = 0;
+        playerLife.style.width = '0%';
+        arena.appendChild(playerLose(player));
+    }
+}
+
+function getRandom(min = 1, max = 20) {
+    console.log(Math.random() * (max - min) + min);
+    return Math.random() * (max - min) + min;
+}
+
+function playerLose(player) {
+    const loseTitle = createElement('div', 'loseTitle');
+    loseTitle.innerText = (player.player === 1 ? player2.name : player1.name) + ' Win';
+    randomBtn.disabled = true;
+    return loseTitle;
+}
+
+function createElement(tag, className) {
+    const el = document.createElement(tag);
+
+    if (className) {
+        el.className = className;
+    }
+    return el;
+}
+
+
